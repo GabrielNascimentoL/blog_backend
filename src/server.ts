8669139e9bf03c63e25bbfaa630d2087cc79e5e2
Gabriel <gabriellacerdanascimento@hotmail.com>
2025-12-17 00:39:50 -0300

    configuring pooling and routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes/index'
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api", routes) 
 
 export default app;
